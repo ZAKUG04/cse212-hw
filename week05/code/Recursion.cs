319fcd2d64e49119787f2034a5e06a0271029211
ZAKUG04 <zambnefi@gmail.com>
2026-04-03 15:50:15 -0500

    week 5

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-
+using System.Collections.Generic;
 public static class Recursion
 {
     /// <summary>
@@ -14,8 +14,9 @@ public static class Recursion
     /// </summary>
     public static int SumSquaresRecursive(int n)
     {
-        // TODO Start Problem 1
-        return 0;
+         if (n <= 0)
+            return 0;
+        return n * n + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -39,7 +40,17 @@ public static class Recursion
     /// </summary>
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
-        // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+        for (int i = 0; i < letters.Length; i++)
+        {
+            char letter = letters[i];
+            string remaining = letters.Remove(i, 1);
+            PermutationsChoose(results, remaining, size, word + letter);
+        }
     }
 
     /// <summary>
@@ -87,19 +98,24 @@ public static class Recursion
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
         // Base Cases
+        if (remember == null)
+        remember = new Dictionary<int, decimal>();
         if (s == 0)
             return 0;
         if (s == 1)
             return 1;
-        if (s == 2)
+         if (s == 2)
             return 2;
         if (s == 3)
             return 4;
+            
+        if (remember.ContainsKey(s))
+            return remember[s];
 
-        // TODO Start Problem 3
-
-        // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember)
+                    + CountWaysToClimb(s - 2, remember)
+                    + CountWaysToClimb(s - 3, remember);
+        remember[s] = ways;
         return ways;
     }
 
@@ -118,7 +134,19 @@ public static class Recursion
     /// </summary>
     public static void WildcardBinary(string pattern, List<string> results)
     {
-        // TODO Start Problem 4
+        int index = pattern.IndexOf('*');
+
+        if (index == -1)
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        string withZero = pattern.Substring(0, index) + "0" + pattern.Substring(index + 1);
+        string withOne = pattern.Substring(0, index) + "1" + pattern.Substring(index + 1);
+
+        WildcardBinary(withZero, results);
+        WildcardBinary(withOne, results);
     }
 
     /// <summary>
@@ -127,17 +155,22 @@ public static class Recursion
     /// </summary>
     public static void SolveMaze(List<string> results, Maze maze, int x = 0, int y = 0, List<ValueTuple<int, int>>? currPath = null)
     {
-        // If this is the first time running the function, then we need
-        // to initialize the currPath list.
-        if (currPath == null) {
+        if (currPath == null)
+        {
             currPath = new List<ValueTuple<int, int>>();
         }
-        
-        // currPath.Add((1,2)); // Use this syntax to add to the current path
 
-        // TODO Start Problem 5
-        // ADD CODE HERE
-
-        // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
+        if (!maze.IsValidMove(currPath, x, y))
+            return;
+        currPath.Add((x, y));
+        if (maze.IsEnd(x, y))
+        {
+            results.Add(currPath.AsString());
+            return; 
+        }
+        SolveMaze(results, maze, x + 1, y, new List<ValueTuple<int, int>>(currPath)); 
+        SolveMaze(results, maze, x - 1, y, new List<ValueTuple<int, int>>(currPath)); 
+        SolveMaze(results, maze, x, y + 1, new List<ValueTuple<int, int>>(currPath)); 
+        SolveMaze(results, maze, x, y - 1, new List<ValueTuple<int, int>>(currPath));  
     }
 }
